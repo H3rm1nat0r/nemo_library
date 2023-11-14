1cf86a33e715e61a56b339f639ec9f407fe33cab
Gunnar Schug <gunnar.schug@humanit.de>
2023-11-14 11:22:31 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 git pull
 source .venv/bin/activate
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade -r requirements.txt
 # pip freeze > requirements.txt 
 pip freeze | awk -F "==" '{ print $1 }' > requirements.txt 

642ad5f3bd87c17c4c74db921b61b50c0e9cd352
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2024-12-10 16:51:05 +0100

    mapping Zwischenschritt

--- nemo_library/features/migman_mapping.py
+++ nemo_library/features/migman_mapping.py
@@ -43,6 +43,15 @@ def updateMappingForMigman(
             additionalFields=additionalFields,
         )
 
+        # collect data
+        collectData(
+            config=config,
+            projectName=projectName,
+            field=field,
+            additionalFields=additionalFields,
+            folderForMappingFiles=folderForMappingFiles,
+        )
+
 
 def createMappingProject(
     config: Config,
@@ -111,3 +120,128 @@ def createMappingImportedColumnns(
                 dataType="string",
                 description="",
             )
+
+
+def collectData(
+    config: Config,
+    projectName: str,
+    field: str,
+    additionalFields: list[str],
+    folderForMappingFiles: None,
+):
+
+    projectList = getProjectList(config=config)["displayName"].to_list()
+    ctefields = {}
+    for project in projectList:
+        fields = collectDataFieldsForProject(
+            config=config,
+            project=project,
+            field=field,
+            additionalFields=additionalFields,
+        )
+        if fields:
+            ctefields[project] = fields
+
+    if len(ctefields) > 0:
+        queryforreport = sqlQuery(ctefields)
+
+def collectDataFieldsForProject(
+    config: Config,
+    project: str,
+    field: str,
+    additionalFields: list[str],
+) -> list[str]:
+
+    fieldList = None
+    if project in ["Business Processes", "Master Data"] or project.startswith(
+        "Mapping "
+    ):
+        return None
+
+    imported_columns = getImportedColumns(config=config, projectname=project)[
+        "displayName"
+    ].to_list()
+    result = next(
+        (
+            entry
+            for entry in imported_columns
+            if re.match(rf"^{re.escape(field)} \(\d{{3}}\)$", entry)
+        ),
+        None,
+    )
+    if result:
+        logging.info(f"Found field '{result}' in project '{project}'")
+
+        fieldList = {field: internal_name(result)}
+
+        # check for additional fields now
+        if additionalFields:
+            for additionalField in additionalFields:
+                result = next(
+                    (
+                        entry
+                        for entry in imported_columns
+                        if re.match(
+                            rf"^{re.escape(additionalField)} \(\d{{3}}\)$", entry
+                        )
+                    ),
+                    None,
+                )
+                if not result:
+                    logging.info(
+                        f"Field '{additionalField}' not found in project '{project}'. Skip this project"
+                    )
+
+                fieldList[additionalField] = internal_name(result)
+
+    # we have found all relevant fields in project. Now we are going to collect data
+    return fieldList
+
+def sqlQuery(ctefields : dict[str,str]) -> str:
+    
+    # setup CTEs to load data from source projects 
+    ctes = []
+    for ctekey, ctevalue in ctefields.items():
+        
+        subselect = [f'{fldvalue} AS "source {fldkey}"' for fldkey, fldvalue in ctevalue.items()]
+        
+        ctes.append(f"""CTE_{internal_name(ctekey)} AS (
+    SELECT DISTINCT
+        {"\n\t,".join(subselect)}
+    FROM 
+        PROJECT_{internal_name(ctekey)}
+)""")
+
+    # create a union for all CTEs
+    globfrags = []
+    for ctekey, ctevalue in ctefields.items():
+        
+        subselect = [f'"source {fldkey}"' for fldkey, fldvalue in ctevalue.items()]
+        
+        globfrags.append(f"""\tSELECT
+    {"\n\t,".join(subselect)}
+    FROM 
+        CTE_{internal_name(ctekey)}""")
+    ctes.append(f"""CTE_ALL AS (
+{"\nUNION ALL\n".join(globfrags)})""")
+                
+    # and finally one for distinct value and join it with potentially existing data
+    
+    # we need to get a list of the fields itself. We assume they are the same in every CTE
+    first_key = next(iter(ctefields))  
+    first_value = ctefields[first_key]  
+    subselect = [f'"source {fldkey}"' for fldkey, fldvalue in first_value.items()]
+    
+    query = f"""WITH {"\n,".join(ctes)}
+,CTE_ALL_DISTINCT AS (
+    SELECT DISTINCT
+        {'\n\t,'.join(subselect)}
+    FROM 
+        CTE_ALL
+)
+SELECT  
+{'\n\t,'.join(subselect)}
+FROM
+    CTE_ALL_DISTINCT
+"""
+    print(query)

354bd1df363dc9aeca7547ddb2b6182f1e74549d
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2024-12-13 07:27:19 +0100

    publish

--- Scripts/Test.sql
+++ Scripts/Test.sql
@@ -3,28 +3,28 @@ WITH CTE_bill_to_addresses__customers_ AS (
         s_adresse_bundesland__022_ AS "S_Adresse.Bundesland"
         ,s_adresse_staat__005_ AS "S_Adresse.Staat"
     FROM 
-        PROJECT_bill_to_addresses__customers_
+        mig.PROJECT_bill_to_addresses__customers_
 )
 ,CTE_ship_to_addresses__customers_ AS (
     SELECT DISTINCT
         s_adresse_bundesland__025_ AS "S_Adresse.Bundesland"
         ,s_adresse_staat__008_ AS "S_Adresse.Staat"
     FROM 
-        PROJECT_ship_to_addresses__customers_
+        mig.PROJECT_ship_to_addresses__customers_
 )
 ,CTE_customers AS (
     SELECT DISTINCT
         s_adresse_bundesland__021_ AS "S_Adresse.Bundesland"
         ,s_adresse_staat__004_ AS "S_Adresse.Staat"
     FROM 
-        PROJECT_customers
+        mig.PROJECT_customers
 )
 ,CTE_suppliers AS (
     SELECT DISTINCT
         s_adresse_bundesland__021_ AS "S_Adresse.Bundesland"
         ,s_adresse_staat__004_ AS "S_Adresse.Staat"
     FROM 
-        PROJECT_suppliers
+        mig.PROJECT_suppliers
 )
 ,CTE_ALL AS (
         SELECT
@@ -60,12 +60,7 @@ UNION ALL
 SELECT
     cte."S_Adresse.Bundesland" as "source S_Adresse.Bundesland"
         ,cte."S_Adresse.Staat" as "source S_Adresse.Staat"
-    , mapping.TARGET_s_adresse_bundesland as "target S_Adresse.Bundesland"
-        ,mapping.TARGET_s_adresse_staat as "target S_Adresse.Staat"
+    , NULL as "target S_Adresse.Bundesland"
+        ,NULL as "target S_Adresse.Staat"
 FROM
     CTE_ALL_DISTINCT cte
-LEFT JOIN
-    mig.PROJECT_mapping_s_adresse_bundesland mapping
-ON  
-    mapping.SOURCE_s_adresse_bundesland = cte."S_Adresse.Bundesland"
-         AND mapping.SOURCE_s_adresse_staat = cte."S_Adresse.Staat"
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 git pull
 conda activate nemo_library
+conda update --all -y
 python --version
 python -m pip install --upgrade pip
 pip install --upgrade -r requirements.txt

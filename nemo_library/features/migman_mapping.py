45db557d3153d6c3b70b3b04515c58d9f8612ff4
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2024-12-13 15:25:24 +0100

    mapping zwischenstand

--- nemo_library/features/migman_mapping.py
+++ nemo_library/features/migman_mapping.py
@@ -28,49 +28,22 @@ def updateMappingForMigman(
     # if not already existing, create folder structure
     initializeFolderStructure(local_project_path)
 
-    for field in mapping_fields:
+    projectList = getProjectList(config=config)["displayName"].to_list()
 
-        additionalFields = (
-            additionalfields[field] if field in additionalfields else None
-        )
+    # create mapping projects and upload data
+    # updateMappings(
+    #     config=config,
+    #     mapping_fields=mapping_fields,
+    #     local_project_path=local_project_path,
+    #     additionalfields=additionalfields,
+    #     projectList=projectList,
+    # )
 
-        # create project if needed
-        projectList = getProjectList(config=config)["displayName"].to_list()
-        projectame = f"Mapping {field}"
-
-        newProject = False
-        if not projectame in projectList:
-            newProject = True
-            createMappingProject(config=config, field=field, projectname=projectame)
-
-            # update list of fields
-            createMappingImportedColumnns(
-                config=config,
-                projectname=projectame,
-                field=field,
-                additionalFields=additionalFields,
-            )
-
-        # if there is data provided (as a CSV-file), we upload this now.
-        # if there is no data AND the project just have been created, we upload source data and create a template file
-        loadData(
-            config=config,
-            projectname=projectame,
-            field=field,
-            additionalFields=additionalFields,
-            local_project_path=local_project_path,
-            newProject=newProject,
-        )
-
-        # collect data
-        collectData(
-            config=config,
-            projectname=projectame,
-            field=field,
-            additionalFields=additionalFields,
-            local_project_path=local_project_path,
-            newProject=False,  # project is not new any longer, we can access it's data (maybe uploaded in former steps)
-        )
+    # apply mappings to related projects
+    applyMapping(
+        config=config,
+        projectList=projectList,
+    )
 
 
 def createMappingProject(
@@ -144,6 +117,7 @@ def loadData(
     additionalFields: list[str],
     local_project_path: str,
     newProject: bool,
+    relatedfields: dict[str, dict[str, str]],
 ) -> None:
 
     # project is new and table does not exist. We have to upload dummy-data to enforce creation of database table
@@ -168,11 +142,12 @@ def loadData(
                 f"file {file_path} for project {file_path} not found. Uploading source data"
             )
 
-            ctefields = getctefields(
-                config=config, field=field, additionalFields=additionalFields
-            )
             queryforreport = sqlQuery(
-                project=projectname, ctefields=ctefields, newProject=newProject
+                config=config,
+                field=field,
+                additionalFields=additionalFields,
+                newProject=newProject,
+                relatedfields=relatedfields,
             )
             createOrUpdateReport(
                 config=config,
@@ -208,7 +183,7 @@ def loadData(
             logging.info(f"upload to project {projectname} completed")
 
 
-def getctefields(
+def getRelatedFields(
     config: Config,
     field: str,
     additionalFields: list[str],
@@ -235,14 +210,15 @@ def collectData(
     additionalFields: list[str],
     local_project_path: str,
     newProject: bool,
+    relatedfields: list[str],
 ):
 
-    ctefields = getctefields(
-        config=config, field=field, additionalFields=additionalFields
-    )
-
     queryforreport = sqlQuery(
-        project=projectname, ctefields=ctefields, newProject=newProject
+        config=config,
+        field=field,
+        additionalFields=additionalFields,
+        newProject=newProject,
+        relatedfields=relatedfields,
     )
     createOrUpdateReport(
         config=config,
@@ -333,11 +309,17 @@ def collectDataFieldsForProject(
     return fieldList
 
 
-def sqlQuery(project: str, ctefields: dict[str, str], newProject: bool) -> str:
+def sqlQuery(
+    config: Config,
+    field: str,
+    additionalFields: list[str],
+    newProject: bool,
+    relatedfields: list[str],
+) -> str:
 
     # setup CTEs to load data from source projects
     ctes = []
-    for ctekey, ctevalue in ctefields.items():
+    for ctekey, ctevalue in relatedfields.items():
 
         subselect = [
             f'{fldvalue} AS "{fldkey}"' for fldkey, fldvalue in ctevalue.items()
@@ -354,7 +336,7 @@ def sqlQuery(project: str, ctefields: dict[str, str], newProject: bool) -> str:
 
     # create a union for all CTEs
     globfrags = []
-    for ctekey, ctevalue in ctefields.items():
+    for ctekey, ctevalue in relatedfields.items():
 
         subselect = [f'"{fldkey}"' for fldkey, fldvalue in ctevalue.items()]
 
@@ -372,8 +354,8 @@ def sqlQuery(project: str, ctefields: dict[str, str], newProject: bool) -> str:
     # and finally one for distinct value and join it with potentially existing data
 
     # we need to get a list of the fields itself. We assume they are the same in every CTE
-    first_key = next(iter(ctefields))
-    first_value = ctefields[first_key]
+    first_key = next(iter(relatedfields))
+    first_value = relatedfields[first_key]
     subselect = [f'"{fldkey}"' for fldkey, fldvalue in first_value.items()]
 
     queryctes = f"""WITH {"\n,".join(ctes)}
@@ -416,3 +398,122 @@ ON
     {'\n\t AND '.join(subselectjoin)}"""
 
     return finalquery
+
+
+def updateMappings(
+    config: Config,
+    mapping_fields: list[str],
+    local_project_path: str,
+    additionalfields: dict[str, str],
+    projectList: list[str],
+) -> None:
+
+    # iterate every given field and check whether to create the appropriate project and upload data
+    for field in mapping_fields:
+
+        additionalFields = (
+            additionalfields[field] if field in additionalfields else None
+        )
+
+        # if project does not exist, create it
+        projectame = f"Mapping {field}"
+        newProject = False
+        if not projectame in projectList:
+            newProject = True
+            createMappingProject(config=config, field=field, projectname=projectame)
+
+            # update list of fields
+            createMappingImportedColumnns(
+                config=config,
+                projectname=projectame,
+                field=field,
+                additionalFields=additionalFields,
+            )
+        else:
+            logging.info(f"project {projectame} found.")
+
+        # if there is data provided (as a CSV-file), we upload this now.
+        # if there is no data AND the project just have been created, we upload source data and create a template file
+        relatedfields = getRelatedFields(
+            config=config, field=field, additionalFields=additionalFields
+        )
+
+        loadData(
+            config=config,
+            projectname=projectame,
+            field=field,
+            additionalFields=additionalFields,
+            local_project_path=local_project_path,
+            newProject=newProject,
+            relatedfields=relatedfields,
+        )
+
+        # collect data
+        collectData(
+            config=config,
+            projectname=projectame,
+            field=field,
+            additionalFields=additionalFields,
+            local_project_path=local_project_path,
+            newProject=False,  # project is not new any longer, we can access it's data (maybe uploaded in former steps)
+            relatedfields=relatedfields,
+        )
+
+
+def applyMapping(
+    config: Config,
+    projectList: list[str],
+) -> None:
+
+    # It might happen, that the user does not run our library will all mapping fields given that exists.
+    # In any case, we have to check for ALL mappings defined to create the right reports. So here we ignore
+    # the mapping fields that have been given - we collect all the mappings by ourselves
+    mappingprojects = [x[len("Mapping "):] for x in projectList if x.startswith("Mapping ")]
+    mappingfieldsall = {}
+    for project in mappingprojects:
+        importedcolumns = getImportedColumns(config=config,projectname=f"Mapping {project}")["displayName"].to_list()
+        mappingfieldsprj = [x[len("source "):] for x in importedcolumns if x.startswith("source ")]
+        mappingfieldsall[project] = mappingfieldsprj
+        
+    # scan all the other projects now whether they contain these fields or not    
+    dataprojects = [x for x in projectList if not x.startswith("Mapping") and not x in ["Business Processes","Master Data"]]
+    for project in dataprojects:
+        importedcolumns = getImportedColumns(config=config,projectname=project)["displayName"].to_list()
+        
+        columnstobemapped = []
+        # check for columns in this project that are mapped
+        for key, value in mappingfieldsall.items():
+            result = next(
+                (
+                    entry
+                    for entry in importedcolumns
+                    if re.match(rf"^{re.escape(key)} \(\d{{3}}\)$", entry)
+                ),
+                None,
+            )
+            if result:
+                columnstobemapped.append(result)
+                
+        print(project,":",columnstobemapped)                
+            
+        
+    # otherProjects = list(set(projectList) - set(mappingprojects))
+
+    # for project in otherProjects:
+    #     if project in ["Business Processes", "Master Data"]:
+    #         continue
+
+    #     # search mapping columns in this project
+    #     importedcolumns = getImportedColumns(config=config,projectname=project)["displayName"].to_list()
+    #     mappingcolumns = []
+    #     # result = next(
+    #     #     (
+    #     #         entry
+    #     #         for entry in imported_columns
+    #     #         if re.match(rf"^{re.escape(field)} \(\d{{3}}\)$", entry)
+    #     #     ),
+    #     #     None,
+    #     # )
+    #     # if result:
+        
+    #     print(project,":", mappingcolumns)

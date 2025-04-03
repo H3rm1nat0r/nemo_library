4d980d2b62b7142bbb576dcc1f2c672ee3d4f4dc
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2025-04-03 09:27:18 +0200

    Add setup script for environment setup and requirements management

---
+++ setup.ps1
@@ -0,0 +1,8 @@
+git pull
+conda activate nemo_library
+conda update --all -y
+python --version
+python -m pip install --upgrade pip
+pip install --upgrade -r requirements.txt
+# pip freeze > requirements.txt
+pip freeze | ForEach-Object { ($_ -split "==")[0] } > requirements.txt

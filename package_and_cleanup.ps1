293173ad21d03465c3386657fa26108a5b268663
Gunnar Schug <Gunnar.Schug@nemo-ai.com>
2025-04-07 09:11:07 +0200

    Add Inno Setup installer script and update packaging process

--- package_and_cleanup.ps1
+++ package_and_cleanup.ps1
@@ -4,16 +4,28 @@ pyinstaller --clean --noconfirm pyinstaller_bootstrapper.spec
 # Define paths
 $distFolder = "dist"
 $exeName = "NemoLibraryUI.exe"
-$zipName = "NemoLibraryUI.zip"
+$issFile = "installer_script.iss"
+$outputInstaller = "Output\NemoLibrarySetup.exe"  # Updated path to match Inno Setup output
 
 # Change to the dist folder
 Set-Location $distFolder
 
-# Compress the .exe file into a .zip archive
-Compress-Archive -Path $exeName -DestinationPath $zipName
-
-# Remove the .exe file after zipping
-Remove-Item $exeName
-
 # Return to the original directory
 Set-Location ..
+
+# Remove the destination file if it already exists
+if (Test-Path "$distFolder\NemoLibrarySetup.exe") {
+    Remove-Item -Path "$distFolder\NemoLibrarySetup.exe" -Force -ErrorAction SilentlyContinue
+}
+
+# Use Inno Setup to create a setup file
+& "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" $issFile
+
+# Move the generated installer to the dist folder
+Move-Item -Path ".\$outputInstaller" -Destination "$distFolder\NemoLibrarySetup.exe"
+
+# Remove the empty Output folder
+Remove-Item -Path "Output" -Recurse -Force -ErrorAction SilentlyContinue
+
+# Remove $exeName from the dist folder
+Remove-Item -Path "$distFolder\$exeName" -Force -ErrorAction SilentlyContinue
